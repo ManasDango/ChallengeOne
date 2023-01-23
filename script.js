2c9347d4e1cbf79d6d8e91bfaec14dc2c08eba94
Kewin Trejo <109179169+ManasDango@users.noreply.github.com>
2023-01-23 17:46:10 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function encriptar(){
 	var texto = document.getElementById("inputtx").value.toLowerCase();
-	var txcifrado = texto.replace(/a/igm,"ai");
-	var txcifrado = txcifrado.replace(/e/igm,"enter");
+	var txcifrado = texto.replace(/e/igm,"enter");
 	var txcifrado = txcifrado.replace(/i/igm,"imes");
+	var txcifrado = txcifrado.replace(/a/igm,"ai");
 	var txcifrado = txcifrado.replace(/o/igm,"ober");
 	var txcifrado = txcifrado.replace(/u/igm,"ufat");
 
-	document.getElementById("muñeco").style.display = "none";
+	document.getElementById("imgm").style.display = "none";
 	document.getElementById("text").style.display = "none";
 	document.getElementById("texta").innerHTML = txcifrado;
 	document.getElementById("copiar").style.display = "show";
@@ -15,13 +15,13 @@ function encriptar(){
 
 function desencriptar(){
 	var texto = document.getElementById("inputtx").value.toLowerCase();
-	var txcifrado = texto.replace(/ai/igm,"a");
-	var txcifrado = txcifrado.replace(/enter/igm,"e");
+	var txcifrado = texto.replace(/enter/igm,"e");
 	var txcifrado = txcifrado.replace(/imes/igm,"i");
+	var txcifrado = txcifrado.replace(/ai/igm,"a");
 	var txcifrado = txcifrado.replace(/ober/igm,"o");
 	var txcifrado = txcifrado.replace(/ufat/igm,"u");
 
-	document.getElementById("muñeco").style.display = "none";
+	document.getElementById("imgm").style.display = "none";
 	document.getElementById("text").style.display = "none";
 	document.getElementById("texta").innerHTML = txcifrado;
 	document.getElementById("copy").style.display = "show";
@@ -29,8 +29,12 @@ function desencriptar(){
 }
 
 function copy(){
-	var contenido = document.querrySelector(#texta);
+	var contenido = document.querySelector("#texta");
 	contenido.select();
-	document.execCommand("cut");
-	alert(!Se copio!);
+	document.execCommand("copy");
+	alert("!Se copio");
+}
+
+function limpiar() {
+    document.getElementById("inputtx").value = "";
 }
